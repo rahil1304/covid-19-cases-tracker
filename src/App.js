414b4ba925298f737509fac5fb99301ea563c468
Rahil Sarvaiya <rahil1304@gmail.com>
2020-06-10 00:58:49 +0530

    3 cards of Global, Recovered, Deaths

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import { Cards, Chart, CountryPicker } from "./components";
+import styles from "./App";
+import { fetchData } from "./api";
+
+class App extends Component {
+  state = {
+    data: {},
+  };
+
+  async componentDidMount() {
+    const fetchedData = await fetchData();
+
+    this.setState({ data: fetchedData });
+  }
+  render() {
+    const { data } = this.state;
+    return (
+      <div>
+        <Cards data={data} />
+        <CountryPicker />
+        <Chart />
+      </div>
+    );
+  }
 }
 
 export default App;
---
+++ src/api/index.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+const url = "https://covid19.mathdro.id/api";
+//const url = "https://api.covid19api.com/summary";
+
+export const fetchData = async () => {
+  try {
+    const {
+      data: { confirmed, recovered, deaths, lastUpdate },
+    } = await axios.get(url);
+
+    const modifiedData = {
+      confirmed,
+      recovered,
+      deaths,
+      lastUpdate,
+    };
+
+    return modifiedData;
+    // console.log(response)
+  } catch (error) {}
+};
