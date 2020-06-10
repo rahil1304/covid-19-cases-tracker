b335a315a14e3d947777ebc0edb309a3d843bf86
Rahil Sarvaiya <rahil1304@gmail.com>
2020-06-11 00:32:55 +0530

    Removed wrong dark mode stuff

--- src/components/DarkModeToggler.jsx
+++ src/components/DarkModeToggler.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from "react";
 import DarkModeToggle from "react-dark-mode-toggle";
-import Themes from "../Themes";
 
 export default () => {
   const [isDarkMode, setIsDarkMode] = useState(() => false);
