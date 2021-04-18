f46c66d6de99f413325c8dcb0a2d0aab68aa2ac8
Rahil Sarvaiya <rahil1304@gmail.com>
2021-04-18 15:40:21 +0530

    removed commented code

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -13,10 +13,6 @@ const NavbarComponent = () => {
         <Nav.Link href='#'>Login</Nav.Link>
         <Nav.Link href='#'>Register</Nav.Link>
       </Nav>
-      {/* <Form inline>
-      <FormControl type="text" placeholder="Search" className="mr-sm-2" />
-      <Button variant="outline-info">Search</Button>
-    </Form> */}
     </Navbar>
   );
 };
