d3b96853194c6a2a0d0da9c7d758fb6adb005913
Rahil Sarvaiya <rahil1304@gmail.com>
2021-05-03 19:09:58 +0530

    added register action and type

--- src/components/auth/Register.js
+++ src/components/auth/Register.js
@@ -3,9 +3,11 @@ import { connect } from "react-redux";
 // import axios from "axios";
 import { Link } from "react-router-dom";
 import { setAlert } from "../../actions/alert";
+import { register } from "../../actions/auth";
+
 import PropTypes from "prop-types";
 
-const Register = ({ setAlert }) => {
+const Register = ({ setAlert, register }) => {
   const [formData, setFormData] = useState({
     name: "",
     email: "",
@@ -25,7 +27,7 @@ const Register = ({ setAlert }) => {
     if (password !== password2) {
       setAlert("Passwords do not match", "danger");
     } else {
-      console.log("SUCESS");
+      register({ name, email, password });
       // const newUser = {
       //   name,
       //   email,
@@ -114,6 +116,7 @@ const Register = ({ setAlert }) => {
 
 Register.propTypes = {
   setAlert: PropTypes.func.isRequired,
+  register: PropTypes.func.isRequired,
 };
 
-export default connect(null, { setAlert })(Register);
+export default connect(null, { setAlert, register })(Register);
